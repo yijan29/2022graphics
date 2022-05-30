547948c542c9323132e5b83dda5d718d808f8c84
yijan29 <79989165+yijan29@users.noreply.github.com>
2022-05-30 16:46:49 +0800

    Add files via upload

---
+++ week15-3_mp3/week15-3.cpp
@@ -0,0 +1,11 @@
+#include "CMP3_MCI.h"
+#include <stdio.h>
+CMP3_MCI mp3;
+int main()
+{
+    mp3.Load("07042111.mp3");
+    mp3.Play();
+    printf("現在正在播放羊的聲音\n");
+    int N;
+    scanf("%d",&N);
+}
